496feb4ca913482f59ef052ca97e3a2287414a19
vabracadabra <149600323+vabracadabra@users.noreply.github.com>
2024-04-11 16:49:02 -0300

    d

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const dataatual = new Date();
 let datahalloween = prompt("data do proximo halloween:");
-datahalloween = new Data(datahalloween+"T23:59:59");
+datahalloween = new Data(datahalloween + "T23:59:59");
 let diasQueFaltam = Math.floor((datahalloween-dataAtual)/86400000);
 document.querySelector("#dias_restantes").textContent = diasQueFaltam;
